9388a20dcdea37e9f24f4cf2c8d082f33592e98a
ompro07 <oscarmprada@gmail.com>
2020-10-30 19:40:48 +0000

    com rotas variáveis e templates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello Word"
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/<nome>')
+def ola_com_nome(nome):
+    return render_template('index.html', nome_pessoa=nome)
